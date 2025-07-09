2e5f5dd84c49d932aaed1de1ffbdb1eae67f4870
banasuko <banasuko0105@gmail.com>
2025-07-09 15:03:52 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -426,7 +426,6 @@ with col1:
                         st.info("利用回数を増やすには、プランのアップグレードが必要です。")
                     else:
                         # Decrement uses in Firestore via auth_utils
-                        # Call update_user_uses_in_firestore_rest
                         if auth_utils.update_user_uses_in_firestore_rest(st.session_state["user"], st.session_state["id_token"]): 
                             image_b_bytes = io.BytesIO() # Create BytesIO object for image
                             Image.open(uploaded_file_b).save(image_b_bytes, format="PNG") # Save uploaded image to BytesIO
